1779cfd89f12b0a12548412e586027d5b0506401
Ta Thanh Dinh <tathanhdinh@gmail.com>
2016-07-10 13:58:38 +0200

    implement trace header parser

--- analyzer/Program.fs
+++ analyzer/Program.fs
@@ -1,21 +1,35 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
-type Instruction<'TAddress, 'TThreadID> = { address: 'TAddress;
-                                            mnemonic_string: string;
-                                            thread_id: 'TThreadID }
+type Instruction<'TAddress, 'TThreadID> = { Address: 'TAddress;
+                                            Mnemonic: string;
+                                            ThreadId: 'TThreadID }
 
-let get_trace_length_x86_64 (trace_file_reader:System.IO.BinaryReader) =
+let parseTraceHeader (traceFileReader:System.IO.BinaryReader) =
+  let addrint_size = traceFileReader.ReadByte ()
+  let bool_size = traceFileReader.ReadByte ()
+  let threadid_size = traceFileReader.ReadByte ()
+  (addrint_size, bool_size, threadid_size)
+
+let getTraceLengthX8664 (traceFileReader:System.IO.BinaryReader) =
  let trace_length:uint64 ref = ref (uint64 0)
- while (trace_file_reader.BaseStream.Position <> trace_file_reader.BaseStream.Length) do
-   let instruction_length = trace_file_reader.ReadUInt64 ()
-   trace_file_reader.BaseStream.Seek(int64 instruction_length, System.IO.SeekOrigin.Current) |> ignore
+ while (traceFileReader.BaseStream.Position <> traceFileReader.BaseStream.Length) do
+   let instruction_length = traceFileReader.ReadUInt64 ()
+   traceFileReader.BaseStream.Seek(int64 instruction_length, System.IO.SeekOrigin.Current) |> ignore
    trace_length := !trace_length + (uint64 1)
- trace_length
-
+ !trace_length
 
 [<EntryPoint>]
-let main argv = 
- printfn "%A" argv
- 0 // return an integer exit code
-
+let main argv =
+  if Array.length argv <> 1 then
+    Printf.printfn "give a serialized trace file from the command line (e.g. analyzer trace_file)"
+    0
+  else
+    use traceFileReader = new System.IO.BinaryReader(System.IO.File.OpenRead(argv.[0]))
+    let (addrint_size, bool_size, threadid_size) = parseTraceHeader traceFileReader
+    Printf.printfn "ADDRINT size: %d\nBOOL size: %d\nTHREADID size: %d" addrint_size bool_size threadid_size
+    // let trace_length = getTraceLengthX8664 traceFileReader
+    // Printf.printfn "number of serialized instructions: %d" trace_length
+    1
+ // printfn "%A" argv
+ // 0 // return an integer exit code
