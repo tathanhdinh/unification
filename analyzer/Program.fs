19ad23e90df7c9ad3ef881dc9a586f45dcddc587
Ta Thanh Dinh <tathanhdinh@gmail.com>
2016-07-12 01:42:51 +0200

    implement parsing for x86 trace: use type constraint

--- analyzer/Program.fs
+++ analyzer/Program.fs
@@ -12,6 +12,16 @@ let parseTraceHeader (traceFileReader:System.IO.BinaryReader) =
   let threadid_size = traceFileReader.ReadByte ()
   (addrint_size, bool_size, threadid_size)
 
+(*=====================================================================================================================*)
+
+let getTraceLengthX86 (traceFileReader:System.IO.BinaryReader) =
+  let trace_length:uint32 ref = ref (uint32 0)
+  while (traceFileReader.BaseStream.Position <> traceFileReader.BaseStream.Length) do
+    let instruction_length = traceFileReader.ReadUInt32 ()
+    traceFileReader.BaseStream.Seek(int64 instruction_length, System.IO.SeekOrigin.Current) |> ignore
+    trace_length := !trace_length + (uint32 1)
+  !trace_length
+
 let getTraceLengthX8664 (traceFileReader:System.IO.BinaryReader) =
  let trace_length:uint64 ref = ref (uint64 0)
  while (traceFileReader.BaseStream.Position <> traceFileReader.BaseStream.Length) do
@@ -20,25 +30,55 @@ let getTraceLengthX8664 (traceFileReader:System.IO.BinaryReader) =
    trace_length := !trace_length + (uint64 1)
  !trace_length
 
+(*=====================================================================================================================*)
+
+let deserializeOpcode<'TAddress when 'TAddress : unmanaged> (traceFileReader:System.IO.BinaryReader) =
+  let opcode_size =
+    match typeof<'TAddress> with
+      | t when t = typeof<uint32> -> int (traceFileReader.ReadUInt32 ())
+      | t when t = typeof<uint64> -> int (traceFileReader.ReadUInt64 ())
+  let opcode_buffer = traceFileReader.ReadBytes opcode_size
+  opcode_buffer
+
 let deserializeOpcodeX8664 (traceFileReader:System.IO.BinaryReader) =
   let opcode_size = traceFileReader.ReadUInt64 ()
   // Printf.printfn "opcode size: %d" opcode_size
-  // let opcode_buffer = Array.zeroCreate (int opcode_size)
   let opcode_buffer = traceFileReader.ReadBytes (int opcode_size)
-  (opcode_size, opcode_buffer)
+  opcode_buffer
+  // (opcode_size, opcode_buffer)
+
+(*=====================================================================================================================*)
+
+let deserializeMnemonic<'TAddress when 'TAddress : unmanaged> (traceFileReader:System.IO.BinaryReader) =
+  let mnemonic_len =
+    match typeof<'TAddress> with
+      | t when t = typeof<uint32> -> int (traceFileReader.ReadUInt32 ())
+      | t when t = typeof<uint64> -> int (traceFileReader.ReadUInt64 ())
+  let mnemonic_str = traceFileReader.ReadBytes mnemonic_len
+  System.Text.Encoding.ASCII.GetString mnemonic_str
 
 let deserializeMnemonicX8664 (traceFileReader:System.IO.BinaryReader) =
   let mnemonic_len = traceFileReader.ReadUInt64 ()
-  // Printf.printfn "mnemonic length: %d" mnemonic_len
   let mnemonic_str = traceFileReader.ReadBytes (int mnemonic_len)
   System.Text.Encoding.ASCII.GetString mnemonic_str
-  // (mnemonic_len, mnemonic_str)
+
+(*=====================================================================================================================*)
+
+let deserializeRegMap<'TAddress> (traceFileReader:System.IO.BinaryReader) =
+  let reg_map_len =
+    match typeof<'TAddress> with
+      | t when t = typeof<uint32> -> int (traceFileReader.ReadUInt32 ())
+      | t when t = typeof<uint64> -> int (traceFileReader.ReadUInt64 ())
+  let reg_map_buffer = traceFileReader.ReadBytes reg_map_len
+  reg_map_buffer
 
 let deserializeRegMapX8664 (traceFileReader:System.IO.BinaryReader) =
   let reg_map_len = traceFileReader.ReadUInt64 ()
   // Printf.printfn "register map length: %d" reg_map_len
   let reg_map_buffer = traceFileReader.ReadBytes (int reg_map_len)
   (reg_map_len, reg_map_buffer)
+
+(*=====================================================================================================================*)
 
 let deserializeMemMapX8664 (traceFileReader:System.IO.BinaryReader) =
   let mem_map_len = traceFileReader.ReadUInt64 ()
