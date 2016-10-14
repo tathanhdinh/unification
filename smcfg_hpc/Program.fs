e4e34da339f8bb52504258332ff54ac24fa386d8
Ta Thanh Dinh <tathanhdinh@gmail.com>
2016-10-14 15:54:39 +0200

    minor

--- smcfg_hpc/Program.fs
+++ smcfg_hpc/Program.fs
@@ -12,18 +12,18 @@ type BaseControlFlow<'TAddress> = Instruction<'TAddress> * Instruction<'TAddress
 //type BaseControlFlows<'TAddress> = Collections.ResizeArray<BaseControlFlow<'TAddress>>
 
 // type ControlFlows<'TAddress> = Collections.ResizeArray<ControlFlow<'TAddress>>
-type ControlFlows<'TAddress when 'TAddress : comparison> = System.Collections.Generic.Dictionary<Instruction<'TAddress>, 
+type ControlFlows<'TAddress when 'TAddress : comparison> = System.Collections.Generic.Dictionary<Instruction<'TAddress>,
                                                                                                  InstructionSet<'TAddress>>
 
-type BaseCfg<'TAddress when 'TAddress : comparison> = QuickGraph.BidirectionalGraph<Instruction<'TAddress>, 
+type BaseCfg<'TAddress when 'TAddress : comparison> = QuickGraph.BidirectionalGraph<Instruction<'TAddress>,
                                                                                     QuickGraph.SEdge<Instruction<'TAddress>>>
 
 
 
-type LocationMap<'TAddress when 'TAddress : comparison> = System.Collections.Generic.Dictionary<'TAddress,
-                                                                                                 Instruction<'TAddress>>
+// type LocationMap<'TAddress when 'TAddress : comparison> = System.Collections.Generic.Dictionary<'TAddress,
+//                                                                                                  Instruction<'TAddress>>
 
-//type SmInstruction<'TAddress> = { Address : 'TAddress; 
+//type SmInstruction<'TAddress> = { Address : 'TAddress;
 //                                  InsIndex : int }
 
 //type Trace<'TAddress when 'TAddress : comparison> = Collections.ResizeArray<SmInstruction<'TAddress>>
@@ -64,7 +64,7 @@ let genericReadInt<'TAddress when 'TAddress : unmanaged> (traceFileReader : Syst
 
 (*================================================================================================================*)
 
-let getTraceLengthGeneric<'TAddress when 'TAddress : unmanaged> (traceFileReader : System.IO.BinaryReader) = 
+let getTraceLengthGeneric<'TAddress when 'TAddress : unmanaged> (traceFileReader : System.IO.BinaryReader) =
   let mutable traceLength = 0u
   while (traceFileReader.BaseStream.Position <> traceFileReader.BaseStream.Length) do
     let insLength = genericReadInt<'TAddress> traceFileReader
@@ -102,7 +102,7 @@ let deserializeMemMapGeneric<'TAddress when 'TAddress : unmanaged> (traceFileRea
 
 (*================================================================================================================*)
 
-let extractBaseControlFlows<'TAddress  when 'TAddress : unmanaged and 
+let extractBaseControlFlows<'TAddress  when 'TAddress : unmanaged and
                                             'TAddress : comparison> (traceFileReader:System.IO.BinaryReader) =
   let mutable instructionCount = 0u
   let programControlFlow = new ControlFlows<_>()
@@ -124,24 +124,24 @@ let extractBaseControlFlows<'TAddress  when 'TAddress : unmanaged and
                            NextAddress = nextAddress;
                            Mnemonic = mnemonicStr;
                            ThreadId = threadId }
-    
+
     match entryPoint with
     | None -> entryPoint <- Some newInstruction
     | Some _ -> ignore ()
 
     match prevInstruction with
     | None -> ignore ()
-    | Some lastInstruction -> 
+    | Some lastInstruction ->
       let currentFlow = (lastInstruction, newInstruction)
       if not (programControlFlow.ContainsKey lastInstruction) then
         programControlFlow.Add(lastInstruction, new InstructionSet<_>([newInstruction]))
       else
         if not (programControlFlow.[lastInstruction].Contains newInstruction) then
-        // if not (programControlFlow.Contains currentFlow) then 
+        // if not (programControlFlow.Contains currentFlow) then
         // // if programControlFlow.BinarySearch(currentFlow) < 0 then
         //   programControlFlow.Add(currentFlow)
           programControlFlow.[lastInstruction].Add newInstruction |> ignore
-    
+
     prevInstruction <- Some newInstruction
   // programControlFlow.Sort()
   (entryPoint, instructionCount, programControlFlow)
@@ -154,16 +154,16 @@ let constructBaseCfgFromControlFlows<'TAddress when 'TAddress : comparison> (con
     for dstIns in controlFlows.[srcIns] do
       baseFlows.Add (srcIns, dstIns)
   let edges = Collections.Seq.map (fun (fromIns, toIns) -> QuickGraph.SEdge(fromIns, toIns)) baseFlows
-//  let edges = controlFlows.ToArray() 
-//            |> Collections.Seq.ofArray 
+//  let edges = controlFlows.ToArray()
+//            |> Collections.Seq.ofArray
 //            |> Collections.Seq.map (fun (fromIns, toIns) -> QuickGraph.SEdge(fromIns, toIns))
   QuickGraph.GraphExtensions.ToBidirectionalGraph edges
 
 (*================================================================================================================*)
 
-let buildBasicBlocks<'TAddress when 'TAddress : comparison> (baseCfg:BaseCfg<'TAddress>) 
+let buildBasicBlocks<'TAddress when 'TAddress : comparison> (baseCfg:BaseCfg<'TAddress>)
                                                             (entryPoint:Instruction<'TAddress>) =
-  let trivialControlFlows = new ControlFlows<_>()                                                              
+  let trivialControlFlows = new ControlFlows<_>()
   let discoveredVertices = new Trace<_>()
   let dfsAlgoInstance = QuickGraph.Algorithms.Search.DepthFirstSearchAlgorithm(baseCfg)
   dfsAlgoInstance.SetRootVertex(entryPoint)
@@ -187,26 +187,26 @@ let buildBasicBlocks<'TAddress when 'TAddress : comparison> (baseCfg:BaseCfg<'TA
       else
         basicBlocks.Add <| List.rev currentBasicBlock
         currentBasicBlock <- [instruction]
-      
+
     if baseCfg.OutDegree(instruction) > 1 then
       basicBlocks.Add <| List.rev currentBasicBlock
       currentBasicBlock <- []
-        
+
   if not currentBasicBlock.IsEmpty then
     basicBlocks.Add <| List.rev currentBasicBlock
   (trivialControlFlows, basicBlocks)
 
 (*================================================================================================================*)
 
-let targetInstructions<'TAddress when 'TAddress : comparison> (basicBlock:BasicBlock<'TAddress>) 
+let targetInstructions<'TAddress when 'TAddress : comparison> (basicBlock:BasicBlock<'TAddress>)
                                                               (baseCfg:BaseCfg<'TAddress>) =
   let lastInstruction = basicBlock.[basicBlock.Length - 1]
   let outControlFlows = baseCfg.OutEdges(lastInstruction)
   Seq.map (fun (edge:QuickGraph.SEdge<Instruction<_>>) -> edge.Target) outControlFlows
 
-let constructBasicBlockCfg<'TAddress when 'TAddress : comparison> (basicBlocks:BasicBlocks<'TAddress>) 
+let constructBasicBlockCfg<'TAddress when 'TAddress : comparison> (basicBlocks:BasicBlocks<'TAddress>)
                                                                   (controlFlows:ControlFlows<'TAddress>) =
-  // controlFlows.Sort()                                                                  
+  // controlFlows.Sort()
   let basicBlockControlFlows = new Collections.ResizeArray<_>()
   for srcBb in basicBlocks do
     for dstBb in basicBlocks do
@@ -222,9 +222,9 @@ let constructBasicBlockCfg<'TAddress when 'TAddress : comparison> (basicBlocks:B
   let basicBlockEdges = Collections.Seq.map (fun (src, dst) -> QuickGraph.SEdge(src, dst)) basicBlockControlFlows
   QuickGraph.GraphExtensions.ToBidirectionalGraph basicBlockEdges
 
-let basicBlockLabel<'TAddress when 'TAddress : unmanaged and 
+let basicBlockLabel<'TAddress when 'TAddress : unmanaged and
                                    'TAddress : comparison> (basicBlock:BasicBlock<'TAddress>) =
-  List.fold (+) "" <| List.map (fun (ins:Instruction<_>) -> 
+  List.fold (+) "" <| List.map (fun (ins:Instruction<_>) ->
                                 Printf.sprintf "%s  %s\l" (hexStringOfValue<'TAddress> ins.Address) ins.Mnemonic) basicBlock
 
 type BasicBlockDotEngine () =
@@ -233,10 +233,10 @@ type BasicBlockDotEngine () =
       System.IO.File.WriteAllText(outputFilename, dotString)
       outputFilename
 
-let printBasicBlockCfg<'TAddress when 'TAddress : unmanaged and 
-                                      'TAddress : comparison> (basicBlockCfg:BasicBlockCfg<'TAddress>) 
+let printBasicBlockCfg<'TAddress when 'TAddress : unmanaged and
+                                      'TAddress : comparison> (basicBlockCfg:BasicBlockCfg<'TAddress>)
                                                               (outputFilename:string) =
-  let graphvizFormat = QuickGraph.Graphviz.GraphvizAlgorithm(basicBlockCfg) 
+  let graphvizFormat = QuickGraph.Graphviz.GraphvizAlgorithm(basicBlockCfg)
   graphvizFormat.FormatVertex.Add(fun args ->
                                     let basicBlock = args.Vertex
                                     args.VertexFormatter.Style <- QuickGraph.Graphviz.Dot.GraphvizVertexStyle.Rounded
@@ -248,7 +248,7 @@ let printBasicBlockCfg<'TAddress when 'TAddress : unmanaged and
 (*================================================================================================================*)
 
 [<EntryPoint>]
-let main argv = 
+let main argv =
   if Array.length argv < 1 then
     Printf.printfn "please give a serialized trace file and/or an output file"
     0
@@ -267,7 +267,7 @@ let main argv =
       Printf.printf "deserializing the trace and extracting the basic flows... "
       // let nativeTrace = deserializeTrace<uint32> traceFileReader
       let anEntryPoint, insCount, programControlFlow = extractBaseControlFlows<uint32> traceFileReader
-      Printf.printfn "done: %u/%u parsed/distinguished instructions." insCount programControlFlow.Keys.Count
+      Printf.printfn "done: %u parsed instructions (%u distinguished)." insCount programControlFlow.Keys.Count
 
       match anEntryPoint with
       | None -> Printf.printfn "trace is empty."
@@ -298,7 +298,7 @@ let main argv =
               argv.[1]
             else
               System.IO.Path.ChangeExtension(argv.[0], ".dot")
-          Printf.printf "write the control flow graph to %s" outputFilename
+          Printf.printfn "write the control flow graph to %s" outputFilename
           ignore <| printBasicBlockCfg basicBlockCfg outputFilename
           // Printf.printfn "done."
 
