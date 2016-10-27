49c7453d7e592bcb578eda2c0c5a94a11088d03b
Ta Thanh Dinh <tathanhdinh@gmail.com>
2016-10-28 01:32:49 +0200

    minor

--- tracer_rio/tracer.c
+++ tracer_rio/tracer.c
@@ -1,19 +1,19 @@
-#include <dr_api.h>
-
-#include <stdio.h>
-
-static void event_exit(void);
-
-DR_EXPORT void dr_client_main(client_id_t id, int argc, const char *argv[])
-{
-  printf("tracer with DynamoRIO\n");
-  dr_set_client_name("tracer", "https://github.com/tathanhdinh/unification");
-  dr_register_exit_event(event_exit);
-}
-
-static void event_exit()
-{
-  printf("tracer with DynamoRIO\n");
-  dr_printf("tracer with DynamoRIO\n");
-  return;
-}
+//#include <dr_api.h>
+//
+//#include <stdio.h>
+//
+//static void event_exit(void);
+//
+//DR_EXPORT void dr_client_main(client_id_t id, int argc, const char *argv[])
+//{
+//  printf("tracer with DynamoRIO\n");
+//  dr_set_client_name("tracer", "https://github.com/tathanhdinh/unification");
+//  dr_register_exit_event(event_exit);
+//}
+//
+//static void event_exit()
+//{
+//  printf("tracer with DynamoRIO\n");
+//  dr_printf("tracer with DynamoRIO\n");
+//  return;
+//}
